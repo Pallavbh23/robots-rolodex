5ccf219b3eb415a12bd1876796e0d7387d09bada
pallavbh23 <pallavbh23@gmail.com>
2020-07-11 16:05:19 +0530

    Updates

---
+++ precache-manifest.5aeb74756f6a277af1dd21acf8ab5c6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1182112e1f320aa56822a236afb0c18",
+    "url": "/robots-rolodex/index.html"
+  },
+  {
+    "revision": "afd55b1dc017a7f26b79",
+    "url": "/robots-rolodex/static/css/main.c4b2cded.chunk.css"
+  },
+  {
+    "revision": "1345f644f07044c17738",
+    "url": "/robots-rolodex/static/js/2.c2d00926.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robots-rolodex/static/js/2.c2d00926.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afd55b1dc017a7f26b79",
+    "url": "/robots-rolodex/static/js/main.b8b267b4.chunk.js"
+  },
+  {
+    "revision": "547cd0d3710c467951f5",
+    "url": "/robots-rolodex/static/js/runtime-main.295bb8f7.js"
+  }
+]);
